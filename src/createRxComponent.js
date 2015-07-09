9930801f10d969fd2d8ce7fb5a68edd3574af501
Andrew Clark <acdlite@me.com>
2015-07-09 13:53:33 -0700

    Remove the need for unwrapLast hack by keeping track of whether the component has mounted

--- src/createRxComponent.js
+++ src/createRxComponent.js
@@ -2,12 +2,6 @@ import { Component } from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import { funcSubject } from './';
 
-function unwrapLast(o$) {
-  let value;
-  o$.subscribe(v => value = v).dispose();
-  return value;
-}
-
 export default function createRxComponent(mapProps, render) {
   return class extends Component {
     constructor(props, context) {
@@ -22,18 +16,20 @@ export default function createRxComponent(mapProps, render) {
       // Sequence of child props
       this.childProps$ = mapProps(this.props$, this.context$);
 
-      // Get the initial child props so it works on the first render
-      // Especially important for server-side rendering
-      this.state = unwrapLast(this.childProps$);
+      // Keep track of whether the component has mounted
+      this.componentHasMounted = false;
+
+      // Subscribe to child prop changes so we know when to re-render
+      this.subscription = this.childProps$.subscribe(
+        childProps =>
+          !this.componentHasMounted
+            ? this.state = childProps
+            : this.setState(childProps)
+      );
     }
 
     componentDidMount() {
-      // Subscribe to child prop changes so we know when to re-render
-      // Skip the first set of props, which were already rendered
-      this.subscription = this.childProps$.skip(1).subscribe(
-        // Use setState to trigger a re-render
-        childProps => this.setState(childProps)
-      );
+      this.componentHasMounted = true;
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
